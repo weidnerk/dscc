39e578b9c92d9e74329d45de872d34e37327e1c5
Kevin Weidner <weidnerk@gmail.com>
2020-09-15 10:48:09 -0400

    Activate changepassword and register unit tests.

--- src/app/changepassword/changepassword.component.spec.ts
+++ src/app/changepassword/changepassword.component.spec.ts
@@ -1,16 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChangepasswordComponent } from './changepassword.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../_services';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogModule } from '@angular/material/dialog';
 
-xdescribe('ChangepasswordComponent', () => {
+describe('ChangepasswordComponent', () => {
   let component: ChangepasswordComponent;
   let fixture: ComponentFixture<ChangepasswordComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChangepasswordComponent ]
+      declarations: [ChangepasswordComponent],
+      imports: [HttpClientTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatDialogModule,
+        RouterModule.forRoot([])],
+      providers: [UserService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { RouterModule } from '@angular/router';
+import { UserService } from '../_services';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-xdescribe('RegisterComponent', () => {
+describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ RegisterComponent ],
+      imports: [HttpClientTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot([])],
+      providers: [UserService]
     })
     .compileComponents();
   }));
